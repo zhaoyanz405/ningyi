e5bce98b36e98cbd646ed277443079f376c6ae6c
zhaoy <zhaoyanz405@gmail.com>
2018-06-04 15:37:29 +0800

    k

--- index.py
+++ index.py
@@ -15,12 +15,23 @@ def home():
 
 @app.route('/news')
 def news():
-    title_list = [ art['title'] for art in article_list]
+    title_list = [art['title'] for art in article_list]
     return render_template('news.html', titles=title_list)
 
-@app.route('/n-detail')
-def detail():
-    return render_template('n-detail.html')
+
+@app.route('/n-detail/<name>')
+def detail(name):
+    print(name)
+    title_list = [art['title'] for art in article_list]
+    print(title_list)
+    for art in article_list:
+        wrong_art = {
+            'title': "没有这篇文章"
+        }
+        if name == art['title']:
+            return render_template('n-detail.html', article=art)
+
+    return render_template('n-detail.html', article=wrong_art)
 
 
 if __name__ == '__main__':
